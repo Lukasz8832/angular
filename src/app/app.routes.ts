52cedc2638338498175e570e473e32cec083e782
unknown <nlukasz8832@gmail.com>
2024-02-28 11:03:47 +0100

    d
    d

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "", 
+        loadChildren:() => import("./pages/login-page/login-page.module").then(m => m.LoginPageModule), 
+    },
+    {
+        path: "/sklep",
+        loadChildren:() => import("./pages/home-page/home-page.module").then(m => m.HomePageModule), 
+    }
+];
