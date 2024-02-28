a34845ed5ae413ee1493997520feeab68e2173d3
unknown <nlukasz8832@gmail.com>
2024-02-28 12:13:07 +0100

    1
    1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,12 @@ import { LoginPageComponent } from './pages/login-page/login-page.component';
 
 export const routes: Routes = [
     {
-        path: "", 
+        path: '', 
         loadChildren:() => import("./pages/login-page/login-page.module").then(m => m.LoginPageModule), 
+        
     },
     {
-        path: "/sklep",
+        path: 'sklep',
         loadChildren:() => import("./pages/home-page/home-page.module").then(m => m.HomePageModule), 
-    }
+    },
 ];
