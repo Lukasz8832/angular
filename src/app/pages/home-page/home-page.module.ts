990b10e051b82556a73b4ec43e09805fe049da32
unknown <nlukasz8832@gmail.com>
2024-02-28 12:33:11 +0100

    11
    1

--- src/app/pages/home-page/home-page.module.ts
+++ src/app/pages/home-page/home-page.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './home-page.component';
 import { RouterModule, Routes } from '@angular/router';
+import { NavigationComponent } from '../../components/navigation/navigation.component';
 
  const routes: Routes = [
   {
@@ -14,6 +15,7 @@ import { RouterModule, Routes } from '@angular/router';
   declarations: [HomePageComponent],
   imports: [
     CommonModule,
+    NavigationComponent,
     RouterModule.forChild(routes)
   ],
   exports: [HomePageComponent]
