dac0bfb8f6cec49dbf48210aa8453130969fb56c
unknown <nlukasz8832@gmail.com>
2024-03-01 10:51:20 +0100

    22
    22

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
   standalone: false,
   templateUrl: './login-page.component.html',
-  styleUrl: './login-page.component.css'
+  styleUrl: './login-page.component.css',
 })
 export class LoginPageComponent {
+  constructor(private router: Router){
 
+  }
+  essa(){
+    document.getElementById("test")!.innerHTML = " 123"
+  }
+  loginForm = new FormGroup({
+    email: new FormControl ('', [Validators.required]),
+    password: new FormControl ('', [Validators.required])
+  })
 }
--- src/app/pages/login-page/login-page.module.ts
+++ src/app/pages/login-page/login-page.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginPageComponent } from './login-page.component';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 const routes: Routes = [
@@ -14,7 +15,9 @@ const routes: Routes = [
   declarations: [LoginPageComponent],
   imports: [
     CommonModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    ReactiveFormsModule,
+    FormsModule
   ],
   exports: [LoginPageComponent]
 })
