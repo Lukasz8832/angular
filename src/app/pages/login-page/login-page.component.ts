24ab9d574868f534a3c7a047e64a444277302f4d
unknown <nlukasz8832@gmail.com>
2024-03-01 11:17:27 +0100

    11
    11

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -12,8 +12,18 @@ export class LoginPageComponent {
   constructor(private router: Router){
 
   }
-  essa(){
-    document.getElementById("test")!.innerHTML = " 123"
+  logowanie(){
+    let loginValue1 = this.loginForm.get("email")!.value
+   let passwordValue1 = this.loginForm.get("password")!.value
+  let loginValue = "Ukasz"
+  let passwordValue = "haslo"
+  let blad = ""
+  if(loginValue1 == loginValue && passwordValue1 == passwordValue ){
+    this.router.navigate(['/sklep'])
+  }
+  else{
+    blad = "Zly email lub haslo"
+  }
   }
   loginForm = new FormGroup({
     email: new FormControl ('', [Validators.required]),
