1073d6837db618a3c711948094285d31ff32c250
unknown <nlukasz8832@gmail.com>
2024-03-04 12:58:50 +0100

    ds
    dsds

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { RestService } from '../../services/rest.service';
 @Component({
   selector: 'app-home-page',
   standalone: false,
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnInit {
+  public essa: Array<any> = []
+  constructor(private apiService: RestService){
+      
+  }
+  ngOnInit() : any {
+    this.apiService.getProdukt().subscribe(
+      (data) => { 
+        this.essa.push[this.data.name]
 
+      });
+  }
 }
